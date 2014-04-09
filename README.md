5c430193a699687927d72264d942d11777552367
Wilson Griffith <griffiw@gmail.com>
2014-04-09 00:51:51 -0400

    Modified required file format, added CLI params.
    Changed the file format to be the definition and word separated by a '--'. Also added command line parameters for which file to study and how many times each word should be repeated.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-japanese-studier
-================
 
-A simple script to help study any language (I personally use it for Japanese, hence the name)
+# Japanese Studier
+
+An extremely simple script to help study any language (I personally use it for Japanese, hence the name)
 
 ## How to use
 The script takes a single command line argument, a text file, e.g.
@@ -11,14 +11,20 @@ The script takes a single command line argument, a text file, e.g.
 
  The text file must follow this format:
 
-     "<definition you want to be asked>" = "<word in whatever language you're studying>"
+     <definition you want to be asked>--<word in whatever language you're studying>
+     ...
+
+For example:
+
+     new--あたらしい
      ...
 
 ## "Features"
+
 * Randomly picks next word rather than using a repetitious sequential order
-* Uses a queue to further increase variety with which words appear
-* Words stop appearing once you've gotten them right 5 times
+* Uses an MRU queue to increase the variety with which words appear
+* Words stop appearing once you've gotten them right 5 times (by default)
 
 ## What's next
 
-The current required format for the text file is obviously a little goofy. I'll probably alter it to be a CSV format instead. I'll also add a command line parameter to set how many times you want to have to get a word right before it stops appearing.
+I'd like to add command line parameters to set how many times you want to have to get a word right before it stops appearing as well as set which side of the text file you're studying.
--- study.rb
+++ study.rb
@@ -1,5 +1,6 @@
-#! /usr/bin/ruby
-abort("Please provide one file of vocabulary terms") if ARGV.count != 1
+#!/usr/bin/ruby
+load 'trollop.rb'
+abort("Please provide one file of vocabulary terms") if ARGV.count < 1
 
 class Hash
    def get_rand_pair
@@ -8,35 +9,44 @@ class Hash
    end
 end
 
+opts = Trollop::options do
+    opt :repetitions, "Number of repetitions for each vocabulary term", :default => 5
+    opt :file, "File containing your vocabulary terms", :type => :string
+end
+Trollop::die :repetitions, "must be greater than or equal to 1" if opts[:repetitions] <= 0
+Trollop::die :file, "must exist" unless File.exist?(opts[:file]) if opts[:file]
+
 vocab = {}
 right_count = {}
-File.open(ARGV[0], "r") do |file|
+File.open(opts[:file], "r") do |file|
     file.each_line do |line|
-        definition_term = line.split(" = ").map { |e| e.gsub(/\"/, "").chomp }
+        definition_term = line.split('--').map { |e| e.chomp }
         vocab[definition_term[0]] = definition_term[1]
         right_count[definition_term[0]] = -1
     end
 end
 
 score = 0
-n = 0
+total = 0
 mru = [] # Queue used to increase variety of which terms are shown
 begin
     puts 'Press Ctrl-C whenever you are done studying.'
     loop {
         term = vocab.get_rand_pair
+
         # Skip current term if user has already gotten it right several times or
         # if the queue still includes that term
-        next if right_count[term[0]] >= 5 || mru.include?(term[0])
+        next if right_count[term[0]] >= opts[:repetitions] || mru.include?(term[0])
 
         mru.push(term[0]) # Add term to most recently seen queue
         print "#{term[0]}? "
+
         # Ignore white space and case
         answer = STDIN.gets.chomp.downcase.gsub(/\s/, '')
         if answer == term[1]
             score += 1
             right_count[term[0]] += 1
-            puts "Correct! (only #{5 - right_count[term[0]]} more times to go)"
+            puts "Correct! (only #{opts[:repetitions] - right_count[term[0]]} more times to go)\n\n"
         else
             puts "Incorrect, the correct answer is \"#{term[1]}\"."
             new_answer = ''
@@ -44,9 +54,9 @@ begin
                 print 'Type the correct answer: '
                 new_answer = STDIN.gets.chomp.downcase.gsub(/\s/, '')
             end
+            puts
         end
-		n += 1
-        puts "#{mru.size}"
+        total += 1
 
         # TODO: Fix bug where all that remains to study is what's already in the
         # "recently seen" queue
@@ -59,9 +69,9 @@ begin
         end
     }
 rescue Interrupt => e
-	unless n == 0
-    	puts "\nYour final score is #{score}/#{n}"
-	else
-		puts # Print an empty line if user did not play
-	end
+    unless total == 0
+        puts "\nYour final score is #{score}/#{n}"
+    else
+        puts # Print an empty line if user did not play
+    end
 end
